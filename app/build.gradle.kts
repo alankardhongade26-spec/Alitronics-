6a9ca70c27b4c41f17e996b5c669df7506748c42
alankardhongade26-spec <alankardhongade26@gmail.com>
2026-09-12 11:42:55 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,13 @@ android {
             isMinifyEnabled = false
         }
     }
-}
 
-kotlin {
-    jvmToolchain(17)
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
