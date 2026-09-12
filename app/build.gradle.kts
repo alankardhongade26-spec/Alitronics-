c6c7727b8ec253ea8e6294be09652adca7d2cdd9
alankardhongade26-spec <alankardhongade26@gmail.com>
2026-09-12 16:44:39 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,4 +32,9 @@ android {
 }
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation("androidx.camera:camera-core:1.4.2")
+    implementation("androidx.camera:camera-camera2:1.4.2")
+    implementation("androidx.camera:camera-lifecycle:1.4.2")
+    implementation("androidx.camera:camera-view:1.4.2")
 }
