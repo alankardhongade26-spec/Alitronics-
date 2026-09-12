1dba9ef37bf0354960eac94245e20f5f4c572654
alankardhongade26-spec <alankardhongade26@gmail.com>
2026-09-12 12:03:57 +0700

    Update MainActivity.kt

--- app/src/main/java/com/alitronics/app/MainActivity.kt
+++ app/src/main/java/com/alitronics/app/MainActivity.kt
@@ -1,1 +1,41 @@
+package com.alitronics.app
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nameInput = findViewById<EditText>(R.id.nameInput)
+        val startButton = findViewById<Button>(R.id.startButton)
+        val videoButton = findViewById<Button>(R.id.videoButton)
+
+        startButton.setOnClickListener {
+            val name = nameInput.text.toString().trim()
+
+            if (name.isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Welcome to Alitronics, $name",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        videoButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Video calling will be added next",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
